b61c3f19cdb3dde186450d76371e7b185bc81486
rasputin1127 <kyle.boerstler@studyliberalarts.org>
2021-06-30 11:27:52 -0400

    Adding comments to audio generator.py

--- src/audio_generator.py
+++ src/audio_generator.py
@@ -1,65 +1,84 @@
 import tensorflow as tf
 import os
-from scipy.io import wavfile
 import librosa
-import pandas as pd
 import numpy as np
-from sklearn.utils.class_weight import compute_class_weight
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from tensorflow.audio import decode_wav
-from tqdm import tqdm
-from glob import glob
-import argparse
-import warnings
-from pydub.utils import mediainfo
 
 
 class DataGenerator(tf.keras.utils.Sequence):
-    def __init__(self, wav_paths, labels, sr, dt, n_classes,
+    def __init__(self, wav_paths, labels, sr, n_classes,
                  batch_size=32, shuffle=True):
+        '''
+        Tensorflow Sequence class to generate and feed audio files and labels into models for training.
+
+        Parameters:
+        wav_paths: List --> list of all full-paths to .wav files for training
+        labels: List --> list of all text that accompanies .wav files for training
+        sr: Int --> desired sample rate for final .wav file data before training
+        n_classes: Int --> number of classes that will exist in y-label set
+        batch_size: Int --> desired batch size for training
+        shuffle: Bool --> whether to shuffle the training data or not
+
+        Output:
+        Two tensors each time it is called of X = (batch size, 40000, 1) and Y = (batch size, n classes)
+        '''
+
         self.wav_paths = wav_paths
         self.labels = labels
         self.sr = sr
-        self.dt = dt
         self.n_classes = n_classes
         self.batch_size = batch_size
         self.shuffle = True
         self.on_epoch_end()
 
-
     def __len__(self):
+        '''
+        Returns number of batches required per epoch.
+        '''
+
         return int(np.floor(len(self.wav_paths) / self.batch_size))
 
-
     def __getitem__(self, index):
+        '''
+        Generates the X, Y tensors for training when called by Tensorflow's API.
+
+        Paramters:
+        index: Int --> indicates which batch is being called, sets indexing appropriately
+
+        Output:
+        X, Y tensors where X is the .wav file data appropriately preprocessed and Y is the accompanying labels
+        '''
+
+        # sets indexes for current batch
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
+        # grabs the appropriate .wav files and accompanying labels
         wav_paths = [self.wav_paths[k] for k in indexes]
         labels = [self.labels[k] for k in indexes]
 
-        # generate a batch of time data
+        # generate a batch of time data holders for audio files and labels
         X = np.empty((self.batch_size, int(100000), 1), dtype=np.float32)
         Y = np.empty((self.batch_size, self.n_classes), dtype=np.float32)
 
+        # loads file, resamples down to appropriate rate, and fixes length of tensors for feeding into model
         for i, (path, label) in enumerate(zip(wav_paths, labels)):
             rate, wav = librosa.load(path)
             resamp = librosa.resample(rate, wav, self.sr)
             fix_length_samp = librosa.util.fix_length(resamp, 40000)
-            X[i,] = fix_length_samp.reshape(-1, 1)
-            Y[i,] = label
-            
+            X[i, ] = fix_length_samp.reshape(-1, 1)
+            Y[i, ] = label
 
         return X, Y
 
-
+    # if shuffle is True, shuffles the data after each epoch
     def on_epoch_end(self):
         self.indexes = np.arange(len(self.wav_paths))
         if self.shuffle:
             np.random.shuffle(self.indexes)
 
 
+# helper function that uses the directory of wav files to populate a master audio files list
+# clip is used so that only audio files under a specified duration will be grabbed
 def get_wav_paths(wav_dir, clip):
     all_wav = []
     mask = []
@@ -68,16 +87,21 @@ def get_wav_paths(wav_dir, clip):
         if item[2]:
             for rec in item[2]:
                 if rec.endswith('.wav'):
-                        wav = item[0] + '/' + rec
-                        sig, freq = librosa.load(wav)
-                        if len(sig)/freq > clip:
-                            idx += 1
-                        else:
-                            all_wav.append(wav)
-                            mask.append(idx)
-                            idx += 1
+                    wav = item[0] + '/' + rec
+                    sig, freq = librosa.load(wav)
+                    if len(sig)/freq > clip:
+                        idx += 1
+                    else:
+                        all_wav.append(wav)
+                        mask.append(idx)
+                        idx += 1
 
+    # returns a mask used in get_labels so that the appropriate labels are grabbed
     return all_wav, mask
+
+# helper function that uses the directory of wav files to populate a master labels list
+# mask is used to ensure labels and audio files match up appropriately
+
 
 def get_labels(dir_path, mask):
     docs = []
@@ -97,7 +121,7 @@ def get_labels(dir_path, mask):
     y_fit = tfidf.fit_transform(y)
     vocab = tfidf.get_feature_names()
     labels = y_fit.toarray()
-    labels[labels>0] = 1
+    labels[labels > 0] = 1
 
+    # vocab is returned so that some models I was testing would have a feature list
     return labels, vocab
-
